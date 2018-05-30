42dc0468125e4fe543290242b49dc3e025b493ed
qq <qq@qq.com>
2018-05-30 17:47:12 +0800

    init

---
+++ mysql/数据库备份/export-all.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+@echo ###################################################################
+@echo		全量备份数据
+@echo ###################################################################
+mysqldump -uroot -proot --quick --all-databases --flush-logs --delete-master-logs --single-transaction > E:/backup/mysqldump.sql
+@pause
